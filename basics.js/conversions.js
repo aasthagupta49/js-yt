d6dffb1d8923c2f064d9eea7675de86c8f042966
aasthagupta49 <122432124+aasthagupta49@users.noreply.github.com>
2024-02-21 10:40:36 +0000

    operations

---
+++ basics.js/conversions.js
@@ -0,0 +1,27 @@
+// let score = "33abc"
+// console.log(typeof score)
+
+// let x = Number(score)
+// console.log(typeof x)
+// console.log(x) //printing NaN
+
+// let y = null
+// console.log(typeof y) //object
+// console.log(y) //null
+
+// let y = Number(null)
+// console.log(typeof y) //number
+// console.log(y) //0
+
+// let z = undefined;
+// console.log(typeof z)
+// console.log(z)
+
+// let z = Number(undefined);
+// console.log(typeof z) //number
+// console.log(z) //NaN
+
+//when we convert to number 
+// "33" - 33
+// "33abc" - NaN
+//true - 1, false - 0
---
+++ basics.js/operations.js
@@ -0,0 +1,14 @@
+let value = 3;
+let neg = -value;
+//console.log(neg)
+
+// console.log(2+2)
+// console.log(2/3) //is not converted to int, gives float value
+// console.log(2%3)
+
+//string and number conversions
+
+// console.log("1" + 2+ 2) //122
+// console.log(1 + 2+ "2") //32
+// console.log(+"") //0
+//prefix and postfix operators
