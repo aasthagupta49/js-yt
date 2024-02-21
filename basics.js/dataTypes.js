406cdc8797cdee8089e5c32a2dbaf9733f5d4459
aasthagupta49 <122432124+aasthagupta49@users.noreply.github.com>
2024-02-21 08:08:17 +0000

    datatypes

---
+++ basics.js/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) not gonna work because we r using node js
+// console.log(3+3)
+// console.log(42/ +0);
+// console.log(42/ -0);
+
+
+//primitve data types //
+//null - standalone value
+//undefined - value not assigned 
+//symbol - to define uniqueness
+//boolean 
+//number
+//string
+//bigint
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+
