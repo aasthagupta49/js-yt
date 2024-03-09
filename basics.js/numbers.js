3c75c03c1c77c9fd89219cd3f6f2223598b6f648
aasthagupta49 <122432124+aasthagupta49@users.noreply.github.com>
2024-03-09 16:19:49 +0000

    numbers

---
+++ basics.js/numbers.js
@@ -0,0 +1,28 @@
+// const balance = new Number(100)
+// console.log(balance) //[Number:100]
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+// const other = 123.896
+// console.log(other.toPrecision(4))
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(-4.6)) //-5
+// console.log(Math.round(4.6)) //5
+// console.log(Math.round(-4.4))//-4
+// console.log(Math.ceil(-4.6)) //-4
+// console.log(Math.ceil(4.6)) //5
+// console.log(Math.floor(4.6)) //4
+
+console.log(Math.random())
+const min = 1;
+const max = 6;
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
